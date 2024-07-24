8de159600cd3016c4adfda466a8de5c8bcf5f002
LexaHeda <pokemontrainer1612@gmail.com>
2024-07-24 18:38:37 +0200

    First

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import os, sys
 
+DECRYPT_KEY = "7e951795cf4cd9942405eeb5f00077cbe35490c2e08d9932d1c2e89f76b56e84"
+
 
 def malware_complete():
     pass
 
 
 def malware_current_folder():
-    pass
+    os.getcwd()
+    files = [entry for entry in os.listdir('.') if os.path.isfile(os.path.join('.', entry))]
 
 
 if __name__ == "__main__":
